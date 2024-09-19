53fb6d1ccf3b60fe6217e8364a982d8f64795f18
DanielT81 <159022715+DanielT81@users.noreply.github.com>
2024-09-19 09:31:43 +0200

    Aktualisieren von brachistochrone_first_try.py

--- brachistochrone_first_try.py
+++ brachistochrone_first_try.py
@@ -1,2 +1,16 @@
 import numpy as np
 import scipy as sp
+
+punkte=np.array[((0,10),(10,0))]
+
+print(punkte[0,1])
+
+
+'''
+Vektor-Errechnung:
+import numpy as np
+v = np.array(entry1) - np.array(entry2)
+
+Sortierung der Liste:
+array_sorted = array[np.argsort(array[:, col_index])]
+'''
