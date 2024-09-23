b925c7ce4b2eb5e1c73de36bb4d30b4c92e61eda
DanielT81 <159022715+DanielT81@users.noreply.github.com>
2024-09-23 02:45:59 +0200

    Add files via upload

---
+++ brachistochrone_first_try.py
@@ -0,0 +1,20 @@
+import numpy as np
+import scipy as sp
+
+punkte=np.array[((0,10),(10,0))]
+
+def vec_sub(minuend, subtrahend, list_name):
+    result = list_name[minuend] - list_name[subtrahend]
+    return(result)
+
+print(vec_sub((1,0),(0,0),punkte))
+
+'''
+Vektor-Errechnung:
+import numpy as np
+v = np.array(entry1) - np.array(entry2)
+
+Sortierung der Liste:
+array_sorted = array[np.argsort(array[:, col_index])]
+
+'''
