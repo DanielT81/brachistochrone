fe71cd21d3b3d4aa3fb038f3a0cf822c9696c699
DanielT81 <faddmebowshot@gmail.com>
2024-10-07 22:30:09 +0200

    physics does not really work in all cases and does not yet spit out a truthful time or end velocity :((

--- TrialAndError2.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-
-print(type(np.roots([1,0,2])[0]))
--- playingaround.py
+++ playingaround.py
@@ -11,21 +11,27 @@ def sqr(var):
     #
 g = 9.81
 def physics(start_vel, def_vec):
+    if start_vel > 0:
+        print('the starting velocity doesnt make sense here')
+        return
     deltaX = def_vec[0]
     deltaY = def_vec[1]
     deltaS = sqrt(sqr(deltaX) + sqr(deltaY))
     cos_phi = sqrt(1/(1+sqr(deltaX / deltaY)))
-    a = (-0.5) * g * cos_phi
+    a = ((-0.5) * g * cos_phi * (-deltaY / sqrt(deltaY**2))) #- (1/2 * 0.1 (air drag coefficient) * area of the object * velocity**2)
     b = start_vel
     c = deltaS
     t = np.roots([a,b,c])
-    if type(t[0]) == 'numpy.complex128' or type(t[1]) == 'numpy.complex128':
+    #if str(t[0])[len(t[0])] == 'j' or str(t[1])[len(t[1])] == 'j':
+    if type(t[0]) == np.complex128 or type(t[1]) == np.complex128:
         print('problem with the imaginary unity')
-    #v1 = start_vel + (g * cos_phi) * t1
-    #v2 = start_vel + (g * cos_phi) * t2
-    #print((-0.5)*g*cos_phi)
+        return
     print(t)
+    t = t[np.argsort(t)]
+    print(t)
+
+    #print(type(t), '\n' * 5, t)
     return t
 
 
-physics(-4,[880,45])
+physics(-100,[-1000,500])
