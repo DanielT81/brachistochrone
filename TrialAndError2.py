2deba59949aa762780b4823f1f36063a74987fc4
DanielT81 <159022715+DanielT81@users.noreply.github.com>
2024-11-23 14:36:41 +0100

    Update TrialAndError2.py

--- TrialAndError2.py
+++ TrialAndError2.py
@@ -1,11 +1,8 @@
-import numpy as np
-from scipy import special
-def drumhead_height(n, k, distance, angle, t):
-   kth_zero = special.jn_zeros(n, k)[-1]
-   return np.cos(t) * np.cos(n*angle) * special.jn(n, distance*kth_zero)
-theta = np.r_[0:2*np.pi:50j]
-radius = np.r_[0:1:50j]
-x = np.array([r * np.cos(theta) for r in radius])
-y = np.array([r * np.sin(theta) for r in radius])
-z = np.array([drumhead_height(1, 1, r, theta, 0.5) for r in radius])
-print(x,y,z)
+arr_time_comp_time():
+    if arr_time[2,6] < arr_time[1,6]:
+        arr_time[1] = arr_time[2]
+    else:
+        if <var> = false:
+            <break>
+        else:
+            <var> = false
