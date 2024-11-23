8c366c12897921cd2c35275d58236067f77d36c2
DanielT81 <159022715+DanielT81@users.noreply.github.com>
2024-11-23 19:16:15 +0100

    Update TrialAndError2.py

--- TrialAndError2.py
+++ TrialAndError2.py
@@ -3,6 +3,8 @@ arr_time_comp_time():
         arr_time[1] = arr_time[2]
     else:
         if <var> = false:
-            <break>
+            return
         else:
             <var> = false
+optimizing_point(start_vel, start_point, end_point, arr_index):
+    
